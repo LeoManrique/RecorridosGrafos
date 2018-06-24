f89db0e57df484da90354af594b5a9072808c997
Leonardo Manrique <leonardomanrique43@gmail.com>
2018-06-24 13:26:07 -0500

    a

--- src/recorridosgrafos/Grafo.java
+++ src/recorridosgrafos/Grafo.java
@@ -21,7 +21,14 @@ public class Grafo<E> {
     public void eliminarVertice(int pos1, int pos2){
         matriz[pos1][pos2] = 0;
     }
-      
+    
+    public void recorridoAnchura(){
+        
+    }
+    public void recorridoProfundidad(){
+        
+    }
+    
     public String toStringElementos(){
         int cantidad = elementos.length;
         String res = "Listado de Elementos: \n";
@@ -45,19 +52,7 @@ public class Grafo<E> {
     
     @Override
     public String toString(){
-        int cantidad = elementos.length;
-        String res = "Listado de Elementos: \n";
-        for (int i = 0; i < cantidad; i++) {
-            res+= "Elemento "+i+": "+elementos[i].getDato()+"\n";
-        }
-        res+="\n";
-        res+= "Matriz de adyacencia: \n";
-        for (int i = 0; i < cantidad; i++) {
-            for (int j = 0; j < cantidad; j++) {
-                res+= matriz[i][j]+"  ";
-            }
-            res+="\n";
-        }
+        String res = toStringElementos()+toStringMatriz();
         return res;
     }
 }
