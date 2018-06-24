e9f903fdcff9402778798306f678c51732674a5f
Leonardo Manrique <leonardomanrique43@gmail.com>
2018-06-24 13:10:05 -0500

    git

--- src/recorridosgrafos/Grafo.java
+++ src/recorridosgrafos/Grafo.java
@@ -19,7 +19,7 @@ public class Grafo<E> {
         matriz[pos1][pos2] = 1;
     }
     public void eliminarVertice(int pos1, int pos2){
-        matriz[pos1][pos2] = 1;
+        matriz[pos1][pos2] = 0;
     }
     
     public Nodo<E>[] getElementos() {
@@ -30,4 +30,20 @@ public class Grafo<E> {
         return matriz;
     }
     
+    @Override
+    public String toString(){
+        int cantidad = elementos.length;
+        String res = "Listado de Elementos: \n";
+        for (int i = 0; i < cantidad; i++) {
+            res+= "Elemento "+i+": "+elementos[i].getDato()+"\n";
+        }
+        res+= "Matriz de adyacencia: \n";
+        for (int i = 0; i < cantidad; i++) {
+            for (int j = 0; j < cantidad; j++) {
+                res+= matriz[i][j]+"  ";
+            }
+            res+="\n";
+        }
+        return res;
+    }
 }
--- src/recorridosgrafos/RecorridosGrafos.java
+++ src/recorridosgrafos/RecorridosGrafos.java
@@ -3,7 +3,14 @@ package recorridosgrafos;
 public class RecorridosGrafos {
 
     public static void main(String[] args) {
+        Nodo<Integer>[] lista = new Nodo[3];
+        lista[0] = new Nodo(0);
+        lista[1] = new Nodo(1);
+        lista[2] = new Nodo(2);
         
+        Grafo grafo = new Grafo(lista);
+        
+        System.out.println(grafo.toString());
     }
     
 }
